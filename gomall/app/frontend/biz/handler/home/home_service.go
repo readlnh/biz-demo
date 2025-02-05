cf4491030c1e9ae6491e689d109f46ef6186d84e
readlnh <readlnh@163.com>
2025-02-05 05:08:02 +0100

    feat: Implement user authentication and home page features
    This commit introduces user authentication and home page functionalities to the frontend application.
    
    The changes include:
    
    Implemented user registration and login features, including service layer, handler layer, and route configurations.
    Added authentication middleware to protect routes that require user login.
    Created sign-in and sign-up page templates.
    Implemented home page service and handler.
    These changes provide basic user authentication and home page functionalities for the frontend application.

--- gomall/app/frontend/biz/handler/home/home_service.go
+++ gomall/app/frontend/biz/handler/home/home_service.go
@@ -7,14 +7,14 @@ import (
 	"github.com/cloudwego/hertz/pkg/protocol/consts"
 	"github.com/readlnh/biz-demo/gomall/app/frontend/biz/service"
 	"github.com/readlnh/biz-demo/gomall/app/frontend/biz/utils"
-	home "github.com/readlnh/biz-demo/gomall/app/frontend/hertz_gen/frontend/home"
+	"github.com/readlnh/biz-demo/gomall/app/frontend/hertz_gen/frontend/common"
 )
 
 // Home .
 // @router / [GET]
 func Home(ctx context.Context, c *app.RequestContext) {
 	var err error
-	var req home.Empty
+	var req common.Empty
 	err = c.BindAndValidate(&req)
 	if err != nil {
 		utils.SendErrResponse(ctx, c, consts.StatusOK, err)
@@ -27,6 +27,6 @@ func Home(ctx context.Context, c *app.RequestContext) {
 		return
 	}
 
-	c.HTML(consts.StatusOK, "home.tmpl", resp)
+	c.HTML(consts.StatusOK, "home", utils.WarpResponse(ctx, c, resp))
 	// utils.SendSuccessResponse(ctx, c, consts.StatusOK, resp)
 }
--- gomall/app/frontend/biz/service/home.go
+++ gomall/app/frontend/biz/service/home.go
@@ -4,7 +4,7 @@ import (
 	"context"
 
 	"github.com/cloudwego/hertz/pkg/app"
-	home "github.com/readlnh/biz-demo/gomall/app/frontend/hertz_gen/frontend/home"
+	"github.com/readlnh/biz-demo/gomall/app/frontend/hertz_gen/frontend/common"
 )
 
 type HomeService struct {
@@ -16,13 +16,13 @@ func NewHomeService(Context context.Context, RequestContext *app.RequestContext)
 	return &HomeService{RequestContext: RequestContext, Context: Context}
 }
 
-func (h *HomeService) Run(req *home.Empty) (map[string]any, error) {
+func (h *HomeService) Run(req *common.Empty) (map[string]any, error) {
 	//defer func() {
 	// hlog.CtxInfof(h.Context, "req = %+v", req)
 	// hlog.CtxInfof(h.Context, "resp = %+v", resp)
 	//}()
 	// todo edit your code
-	var resp = make(map[string]any)
+	resp := make(map[string]any)
 	items := []map[string]any{
 		{"Name": "T-shirt-1", "Price": 100, "Picture": "/static/image/t-shirt-1.jpeg"},
 		{"Name": "T-shirt-2", "Price": 110, "Picture": "/static/image/t-shirt-1.jpeg"},
